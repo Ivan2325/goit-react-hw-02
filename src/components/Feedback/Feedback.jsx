31acc0fddfd76dd2d598757d359609a5f8fd4081
Ivan2325 <167812839+Ivan2325@users.noreply.github.com>
2024-11-04 12:46:32 +0200

    -

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+function Feedback({ feedback, totalFeedback, positiveFeedbackPercentage }) {
+  return (
+    <div>
+      <h2>Feedback Statistics</h2>
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total Feedback: {totalFeedback}</p>
+      <p>Positive Feedback: {positiveFeedbackPercentage}%</p>
+    </div>
+  );
+}
+
+export default Feedback;
